19489f86bc7fbda4ad5374fef880bfdd58feecd6
Ganesh Bhosale <gdb.sci123@gmail.com>
2017-02-13 08:23:22 +0530

    Morphological Gradient

---
+++ res/ip_closing.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('j_noise2.png', 0)
+kernel = np.ones((5,5), np.uint8)
+closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(closing),plt.title('Closing')
+plt.xticks([]), plt.yticks([])
+plt.show()
---
+++ res/ip_opening.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('j_noise.png', 0)
+kernel = np.ones((5,5), np.uint8)
+opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(opening),plt.title('Opening')
+plt.xticks([]), plt.yticks([])
+plt.show()
